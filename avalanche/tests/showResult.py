d3a0962e895a5276892b262b9eb1f26c35c897e2
Alessandro-Taglieri <alessandro.taglieri@ibm.com>
2021-11-22 22:51:39 +0100

    added system metrics with WandB and loss & accuracy on trained tasks

--- avalanche/tests/showResult.py
+++ avalanche/tests/showResult.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import mean
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,8 +13,12 @@ def plotResults(results, n_exp, name, nc = True):
     fwt_avg_str = 'StreamForwardTransfer/eval_phase/test_stream'
     forgetting_avg_str = 'StreamForgetting/eval_phase/test_stream'
     df_acc = pd.DataFrame()
+    df_acc2 = pd.DataFrame()
+    df_loss2 = pd.DataFrame()
     df_loss = pd.DataFrame()
     df_btw = pd.DataFrame()
+    acc2_avg = []
+    loss2_avg = []
     bwt_avg = []
     fwt_avg = []
     forgetting_avg = []
@@ -22,6 +27,8 @@ def plotResults(results, n_exp, name, nc = True):
         acc = []
         loss = []
         btw = []
+        acc2 = []
+        loss2 = []
         fwt_avg.append(results[exp][fwt_avg_str])
         forgetting_avg.append(results[exp][forgetting_avg_str])
         bwt_avg.append(results[exp][bwt_avg_str])
@@ -39,6 +46,17 @@ def plotResults(results, n_exp, name, nc = True):
                     btw.append(results[exp][btw_str+s2])
                 else:
                     btw.append(0)
+
+                ########
+                
+                if i <= exp:
+                    
+                    s3 = 'Task00' +str(i)
+                    acc2.append(results[exp][accuracy_str+s3])
+                    loss2.append(results[exp][loss_str+s3])
+                else:
+                    loss2.append(0)
+                    acc2.append(0)
             else:
                 s = 'Task000'
                 
@@ -48,7 +66,14 @@ def plotResults(results, n_exp, name, nc = True):
         if nc:
           df_btw['after exp_' + str(exp)] = btw
         df_acc['after exp_' + str(exp)] = acc
+        df_acc2['after exp_' + str(exp)] = acc2
+        df_loss2['after exp_' + str(exp)] = loss2
         df_loss['after exp_' + str(exp)] = loss
+
+        acc2_avg.append(mean(acc2[0:exp+1]))
+        loss2_avg.append(mean(loss2[0:exp+1]))
+        
+    
     if nc:
         print('\n####################################################################################################################################')
         print('\nThe following table contains BTW on test set after training x Experience. Each column stays for x exp trained.')
@@ -74,7 +99,7 @@ def plotResults(results, n_exp, name, nc = True):
     if nc:
         fig.savefig('plotMetrics/singlePlot/task_incremental/bwt/'+name+'_BWTmetric.png', dpi=fig.dpi)
     else:
-        fig.savefig('plotMetrics/singlePlot/domain_incremental/bwt'+name+'_BWTmetric.png', dpi=fig.dpi)
+        fig.savefig('plotMetrics/singlePlot/domain_incremental/bwt/'+name+'_BWTmetric.png', dpi=fig.dpi)
     
     print('\n\n####################################################################################################################################')
     print('\nThe following plot shows the behaviour of the average FORGETTING in test set during training over all experience')
@@ -120,12 +145,65 @@ def plotResults(results, n_exp, name, nc = True):
     print('\n####################################################################################################################################')
     print('\nThe following table contains accuracy and loss values obtained on test set after training x Experience. Each column stays for x exp trained.')
     columns = ['Exp_' +str(i) for i in range(n_exp)]
-    df = pd.DataFrame([res_acc, res_loss], columns = columns)
-    df.index = ['Acuracy', 'Loss']
+    
+    if nc:
+        list_values = [acc2_avg, loss2_avg, res_acc, res_loss]
+        list_labels = ['Accuracy avg on past task','Loss avg on past task',' Global Accuracy', 'Loss']
+    else:
+        list_values = [res_acc, res_loss]
+        list_labels = [' Global Accuracy', 'Loss']
+    df = pd.DataFrame(list_values, columns = columns)
+    df.index = list_labels
+
     display(df)
     
+    
+    if nc:
+
+    
+        print('\n\n####################################################################################################################################')
+        print('\nThe following plot shows the behaviour of the accuracy in test set during training over all experience trained beforethe current experience')
+        
+        
+        
+        
+        fig = plt.figure(figsize=(12, 8), dpi=80)
+        plt.plot(x_exp, acc2_avg, label = 'accuracy')
+        plt.title('Accuracy avg for first x Experiences')
+        plt.xlabel('Experience')
+        plt.ylabel('Accuracy metric ')
+        plt.xticks(x_exp)
+        plt.ylim(0.2,0.7)
+        plt.grid()
+        plt.show()
+        
+        fig.savefig('plotMetrics/singlePlot/task_incremental/accuracy/'+name+'_AccuracyMetric_before.png', dpi=fig.dpi)
+        
+
+
+        print('\n\n####################################################################################################################################')
+        print('\nThe following plot shows the behaviour of the loss in test set during training over all experience trained beforethe current experience')
+        
+        
+        
+        
+        fig = plt.figure(figsize=(12, 8), dpi=80)
+        plt.plot(x_exp, loss2_avg, label = 'accuracy')
+        plt.title('Loss avg for first x Experiences')
+        plt.xlabel('Experience')
+        plt.ylabel('Loss metric ')
+        plt.xticks(x_exp)
+        
+        plt.grid()
+        plt.show()
+        fig.savefig('plotMetrics/singlePlot/task_incremental/loss/'+name+'_LossMetric_before.png', dpi=fig.dpi)
+       
+
+
+
     print('\n\n####################################################################################################################################')
     print('\nThe following plot shows the behaviour of the accuracy in test set during training over all experience')
+    
     
     
     res_acc = list(df_acc.mean())
@@ -164,6 +242,8 @@ def plotResults(results, n_exp, name, nc = True):
     result['bwt'] = bwt_avg
     result['forgetting'] = forgetting_avg
     result['fwt'] = fwt_avg
+    result['accuracy_before'] = acc2_avg
+    result['loss_before'] = loss2_avg
 
 
     return result
